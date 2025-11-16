a6d625d99c200fafcb8b26eb86cf969e44fc4e8a
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-11-17 03:48:06 +0530

    Slider Tabs: light mode

--- components/SliderTabs.tsx
+++ components/SliderTabs.tsx
@@ -7,8 +7,8 @@ const SliderTabs: React.FC = () => {
   const [activeTab, setActiveTab] = React.useState(0)
 
   return (
-    <div className="flex flex-col items-center justify-center gap-6 text-white">
-      <div className="grid h-64 w-96 place-content-center rounded-lg bg-black p-4 text-center text-sm transition-all ease-out">
+    <div className="flex flex-col items-center justify-center gap-6">
+      <div className="grid h-64 w-96 place-content-center rounded-lg p-4 text-center text-sm transition-all ease-out">
         <Tabs
           className="text-zinc-200"
           tabsOptions={["Tab 1", "Tab 2", "Tab 3"]}
