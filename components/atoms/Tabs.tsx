0188b2e33a62c43534f1e17b089218e4f9d91b26
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-11-17 03:47:37 +0530

    Tabs: light mode

--- components/atoms/Tabs.tsx
+++ components/atoms/Tabs.tsx
@@ -26,12 +26,12 @@ const Tabs: React.FC<Props> = ({
     >
       <div
         style={highlightStyles}
-        className="absolute left-0 h-full rounded-lg bg-zinc-800 transition-all duration-300"
+        className="absolute left-0 h-full rounded-lg bg-zinc-200 transition-all duration-300"
       />
       {tabsOptions.map((tab, index) => (
         <button
           key={index}
-          className="relative inline-block px-3.5 py-1.5 outline-hidden"
+          className="relative inline-block px-3.5 py-1.5 outline-hidden text-zinc-900"
           onClick={() => {
             setActiveTab(index)
           }}
