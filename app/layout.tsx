155dc4a04e359403d949e0727fcb8125fa8cc2cc
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2026-01-15 13:02:38 +0530

    Poker Widget added

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,7 +45,7 @@ const components = [
   { title: "Jelly Tags", link: "/jelly-tags/" },
   { title: "Slider Tabs", link: "/slider-tabs/" },
   { title: "Airbnb Cards", link: "/airbnb-cards/" },
-  // { title: "Poker Widget", link: "/poker-widget/" },
+  { title: "Poker Widget", link: "/poker-widget/" },
   { title: "Breaking Bulb", link: "/breaking-bulb/" },
   { title: "Upvote Button", link: "/upvote-button/" },
   { title: "Exit Animation", link: "/exit-animation/" },
---
+++ app/poker-widget/page.tsx
@@ -0,0 +1,22 @@
+import * as React from "react"
+
+import { type Metadata } from "next"
+
+import PokerWidget from "@/components/PokerWidget"
+import getMetadata from "@/utils/metadata"
+
+export const metadata: Metadata = getMetadata({
+  path: "/poker-widget/",
+  title: "Poker Widget | Interactive Interfaces",
+  description: "Poker Widget animations",
+})
+
+const UpvoteButtonPage: React.FC = () => {
+  return (
+    <main className="grid h-full place-content-center">
+      <PokerWidget />
+    </main>
+  )
+}
+
+export default UpvoteButtonPage
